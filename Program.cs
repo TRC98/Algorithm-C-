f4f597bbf26147451be5e4bb0549aab7b5422e97
rajithac <rajithac.chamod@synapsys.lk>
2024-07-07 15:58:14 +0530

    ssssssssssss

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
-namespace bublesort{
-    class Program{
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            //Console.ReadLine();
-            while (true) {
-                Console.WriteLine("");
-            }
-        }
-    }
+// namespace bublesort{
+//     class Program{
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, World!");
+//             Console.ReadLine();
+//             //while (true) {
+//             //    Console.WriteLine("");
+//             //}
+//         }
+//     }
+// }
+
+try
+{
+    Log.Information("Start Console");
+
+    var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true,true);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("======================================================================");
+    Console.WriteLine("============== | ** Algorithm Console application ** | ===============");
+    Console.WriteLine("======================================================================");
+    Console.WriteLine("\n");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+}catch(Exception ex){
+    Console.WriteLine(ex.Message);
 }
